0524dde43b45e95c778551d56a2d078f24c2f4be
wusnoop <wusnoop86@gmail.com>
2021-04-20 21:12:46 +0300

    1

--- src/js/script.js
+++ src/js/script.js
@@ -127,10 +127,11 @@ $(window).scroll(function() {
   }
 });
 
-$("a[href^='#']").click(function(){
+$("a[href=#up]").click(function(){
   var _href = $(this).attr("href");
   $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
   return false;
 });
+new WOW().init();
 
 });
